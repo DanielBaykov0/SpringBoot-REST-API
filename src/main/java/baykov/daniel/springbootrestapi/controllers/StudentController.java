807150d426f3a334fcab1667e359ef3fb2a550ea
Daniel Baykov <danibaikov@gmail.com>
2023-06-15 20:53:49 +0300

    adding Spring Boot REST API that returns Java Bean as JSON

---
+++ src/main/java/baykov/daniel/springbootrestapi/controllers/StudentController.java
@@ -0,0 +1,15 @@
+package baykov.daniel.springbootrestapi.controllers;
+
+import baykov.daniel.springbootrestapi.bean.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    // http://localhost:8080/student
+    @GetMapping("/student")
+    public Student getStudent() {
+        return new Student(1, "Ramesh", "Fadatare");
+    }
+}
