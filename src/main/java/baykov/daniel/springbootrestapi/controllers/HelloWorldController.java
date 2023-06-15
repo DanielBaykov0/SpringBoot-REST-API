3c2a174b945956cc81d49a3b37fd10444e329fd1
Daniel Baykov <danibaikov@gmail.com>
2023-06-15 20:46:10 +0300

    adding @GetMapping

---
+++ src/main/java/baykov/daniel/springbootrestapi/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package baykov.daniel.springbootrestapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    // HTTP GET Request
+    // http://localhost:8080/hello-world
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
