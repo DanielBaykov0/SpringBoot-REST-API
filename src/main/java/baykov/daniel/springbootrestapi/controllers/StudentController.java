ed2f5b2190149640bf5258b3fb6f7785f97212d3
Daniel Baykov <danibaikov@gmail.com>
2023-06-15 21:13:04 +0300

    adding Spring Boot REST API with Request Param - @RequestParam

--- src/main/java/baykov/daniel/springbootrestapi/controllers/StudentController.java
+++ src/main/java/baykov/daniel/springbootrestapi/controllers/StudentController.java
@@ -3,6 +3,7 @@ package baykov.daniel.springbootrestapi.controllers;
 import baykov.daniel.springbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -35,5 +36,14 @@ public class StudentController {
                                        @PathVariable("first-name") String firstName,
                                        @PathVariable("last-name") String lastName) {
         return new Student(studentId, firstName, lastName);
+    }
+
+    // Spring Boot REST API with Request Param
+    // http://localhost:8080/students/query?id=1&firstName=Ramesh&lastName=Fadatare
+    @GetMapping("students/query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          @RequestParam String firstName,
+                                          @RequestParam String lastName) {
+        return new Student(id, firstName, lastName);
     }
 }
