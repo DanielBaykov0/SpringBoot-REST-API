80b8e1734980668e8ac9fba0fc59f8ce94b4ceba
Daniel Baykov <danibaikov@gmail.com>
2023-06-15 20:57:52 +0300

    adding Spring Boot REST API that returns List as JSON

--- src/main/java/baykov/daniel/springbootrestapi/controllers/StudentController.java
+++ src/main/java/baykov/daniel/springbootrestapi/controllers/StudentController.java
@@ -4,6 +4,9 @@ import baykov.daniel.springbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -11,5 +14,15 @@ public class StudentController {
     @GetMapping("/student")
     public Student getStudent() {
         return new Student(1, "Ramesh", "Fadatare");
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Ramesh", "Fadatare"));
+        students.add(new Student(2, "Umesh", "Fadatare"));
+        students.add(new Student(3, "Ram", "Jadhav"));
+        students.add(new Student(4, "Sanjay", "Pawar"));
+        return students;
     }
 }
